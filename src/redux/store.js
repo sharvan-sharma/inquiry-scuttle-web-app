7bff5ae0b3c4be1895bf0cfb46dd3cd3bb3e1132
sharvan-sharma <sharvansharma518@gmail.com>
2020-07-01 19:41:17 +0530

    po-d-1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import {createStore,applyMiddleware} from 'redux'
 import rootReducer from './root.reducer'
-import logger from 'redux-logger'
+//import logger from 'redux-logger'
 
-const middlewares = [logger]
+//const middlewares = [logger]
 
-const store = createStore(rootReducer,applyMiddleware(...middlewares))
+//const store = createStore(rootReducer,applyMiddleware(...middlewares))
+const store = createStore(rootReducer)
 
 export default store
